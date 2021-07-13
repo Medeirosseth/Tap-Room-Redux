4ad0ed1b3be3fe2d34ad63f78b6f34ccc0b4834e
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-07-12 18:52:14 -0700

    add passing test *Green Test for Toggling State

--- beer/src/reducers/form-visible-reducer.js
+++ beer/src/reducers/form-visible-reducer.js
@@ -1,3 +1,8 @@
-export default (state = {}, action) => {
-  return state;
-}
+export default (state = {}, action ) =>{
+  switch (action.type){
+    case 'TOGGLE_FORM':
+      return !state;
+    default:
+      return state;
+  }
+};
