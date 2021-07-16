4d1dbe737f5f988b1e3e0b36f970ab4ea6e58bd8
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-07-16 11:08:05 -0700

    add passing test for DELETE KEG

--- beer/src/__tests__/reducers/keg-list-reducer.test.js
+++ beer/src/__tests__/reducers/keg-list-reducer.test.js
@@ -3,6 +3,16 @@ import kegListReducer from '../../reducers/keg-list-reducer';
 describe('kegListReducer', () => {
 
   let action 
+
+  const currentState = {
+    brewery: "TEST BREWERY 2",
+    name: "TEST NAME 2",
+    aC: "TEST ALCOHOL CONTENT 2",
+    price: "TEST PRICE 2",
+    numOfPints: 10,
+    id: 1
+  }
+
   const kegData = {
     brewery: "TEST BREWERY",
     name: "TEST NAME",
@@ -34,5 +44,21 @@ describe('kegListReducer', () => {
       }
     })
   })
+
+  test('Should successfully delete a keg', () => {
+    action = {
+        type: 'DELETE_KEG',
+        id: 1
+    };
+    expect(kegListReducer(currentState, action)).toEqual({
+        2: {   brewery: "TEST BREWERY 2",
+        name: "TEST NAME 2",
+        aC: "TEST ALCOHOL CONTENT 2",
+        price: "TEST PRICE 2",
+        numOfPints: 10,
+        id: 1
+      }
+    });
+  });
   
 });
